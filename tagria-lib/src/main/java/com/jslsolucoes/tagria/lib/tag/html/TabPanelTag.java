0fa64e3c707a1eb33ef05f0a98936dfeee2b516e
jonatan@jslsolucoes.com <jonatan@jslsolucoes.com>
2019-01-22 20:33:43 -0200

    add tab component

---
+++ tagria-lib/src/main/java/com/jslsolucoes/tagria/lib/tag/html/TabPanelTag.java
@@ -0,0 +1,73 @@
+
+package com.jslsolucoes.tagria.lib.tag.html;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import com.jslsolucoes.tagria.lib.html.Attribute;
+import com.jslsolucoes.tagria.lib.html.Div;
+import com.jslsolucoes.tagria.lib.html.Element;
+import com.jslsolucoes.tagria.lib.html.Script;
+import com.jslsolucoes.tagria.lib.html.Ul;
+import com.jslsolucoes.tagria.lib.util.TagUtil;
+
+public class TabPanelTag extends SimpleTagSupport {
+
+	private List<Element> lis = new ArrayList<>();
+	private List<Element> divs = new ArrayList<>();
+
+	@Override
+	public void doTag() throws JspException, IOException {
+
+		TagUtil.flushBody(getJspBody());
+
+		Div div = new Div();
+		div.add(Attribute.ID, TagUtil.getId());
+
+		Ul ul = new Ul();
+		ul.add(Attribute.CLASS, "nav nav-tabs");
+		ul.add(lis);
+		div.add(ul);
+
+		Div tabs = new Div();
+		tabs.add(Attribute.CLASS, "tab-content");
+		tabs.add(divs);
+		div.add(tabs);
+
+		TagUtil.out(getJspContext(), div);
+
+		Script script = new Script();
+		script.add(Attribute.TYPE, "text/javascript");
+		script.add("$('#" + div.get(Attribute.ID) + "').tab();");
+		TagUtil.out(getJspContext(), script);
+
+	}
+
+	public void addLi(Element element) {
+		lis.add(element);
+	}
+
+	public void addDiv(Element element) {
+		divs.add(element);
+	}
+
+	public List<Element> getLis() {
+		return lis;
+	}
+
+	public void setLis(List<Element> lis) {
+		this.lis = lis;
+	}
+
+	public List<Element> getDivs() {
+		return divs;
+	}
+
+	public void setDivs(List<Element> divs) {
+		this.divs = divs;
+	}
+}
---
+++ tagria-lib/src/main/java/com/jslsolucoes/tagria/lib/tag/html/TabTag.java
@@ -0,0 +1,118 @@
+
+package com.jslsolucoes.tagria.lib.tag.html;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.jslsolucoes.tagria.lib.html.A;
+import com.jslsolucoes.tagria.lib.html.Attribute;
+import com.jslsolucoes.tagria.lib.html.Div;
+import com.jslsolucoes.tagria.lib.html.Iframe;
+import com.jslsolucoes.tagria.lib.html.Li;
+import com.jslsolucoes.tagria.lib.html.Script;
+import com.jslsolucoes.tagria.lib.util.TagUtil;
+
+public class TabTag extends SimpleTagSupport {
+
+	private String label;
+	private String url;
+	private Boolean rendered = Boolean.TRUE;
+	private Boolean active = Boolean.FALSE;
+	private Boolean reloadOnSelect = Boolean.TRUE;
+
+	@Override
+	public void doTag() throws JspException, IOException {
+		if (rendered != null && rendered) {
+			TabPanelTag panel = (TabPanelTag) findAncestorWithClass(this, TabPanelTag.class);
+
+			String id = TagUtil.getId();
+			Li li = new Li();
+			if (reloadOnSelect) {
+				li.add(Attribute.CLASS, "ui-tabs-reload-on-select nav-item");
+			}
+			if (active) {
+				li.add(Attribute.CLASS, "active");
+			}
+			
+			A a = new A();
+			a.add(Attribute.CLASS, "nav-link");
+			a.add(Attribute.HREF, "#" + id);
+			a.add(Attribute.DATA_TOGGLE, "tab");
+			a.add(TagUtil.getLocalized(label, getJspContext()));
+			li.add(a);
+
+			panel.addLi(li);
+
+			Div div = new Div();
+			div.add(Attribute.CLASS, "tab-pane m-5");
+			if (active) {
+				div.add(Attribute.CLASS, "active");
+			}
+			div.add(Attribute.ID, id);
+			if (StringUtils.isEmpty(url)) {
+				div.add(TagUtil.getBody(getJspBody()));
+			} else {
+				Iframe iframe = new Iframe();
+				iframe.add(Attribute.CLASS,"iframe");
+				iframe.add(Attribute.ID, TagUtil.getId());
+				iframe.add(Attribute.SRC, TagUtil.getPathForBlank(getJspContext()));
+				iframe.add(Attribute.DATA_URL, TagUtil.getPathForUrl(getJspContext(), url));
+				if (active) {
+					iframe.add(Attribute.CLASS, "active");
+				}
+				div.add(iframe);
+
+				Script script = new Script();
+				script.add(Attribute.TYPE, "text/javascript");
+				script.add("$('#" + iframe.get(Attribute.ID) + "').iframe();");
+				div.add(script);
+			}
+			panel.addDiv(div);
+		}
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Boolean getRendered() {
+		return rendered;
+	}
+
+	public void setRendered(Boolean rendered) {
+		this.rendered = rendered;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public Boolean getReloadOnSelect() {
+		return reloadOnSelect;
+	}
+
+	public void setReloadOnSelect(Boolean reloadOnSelect) {
+		this.reloadOnSelect = reloadOnSelect;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
