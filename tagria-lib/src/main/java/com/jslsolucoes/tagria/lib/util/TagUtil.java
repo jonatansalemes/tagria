543003b32d445f1d5fe77cb6c8e012696d0f6765
Jonatan Lemes <jonatan@jlsolucoes.com>
2019-08-13 17:34:27 -0300

    prepare to release

--- tagria-lib/src/main/java/com/jslsolucoes/tagria/lib/util/TagUtil.java
+++ tagria-lib/src/main/java/com/jslsolucoes/tagria/lib/util/TagUtil.java
@@ -41,7 +41,7 @@ import com.jslsolucoes.tagria.lib.tag.x.StringUtil;
 
 public class TagUtil {
 
-	public static final String VERSION = "3.0.12.0";
+	public static final String VERSION = "3.0.13.0";
 	private static Logger logger = LoggerFactory.getLogger(TagUtil.class);
 
 	private TagUtil() {
