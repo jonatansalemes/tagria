9cfac6d50d16e46b1146afd9e9556d0ab1cad66c
jonatan <jonatan@jslsolucoes.com>
2019-10-28 21:56:29 -0200

    remove converters

--- tagria-spring-v4/src/main/java/com/jslsolucoes/tagria/doc/v4/spring/DefaultWebMvcConfigurerAdapter.java
+++ tagria-spring-v4/src/main/java/com/jslsolucoes/tagria/doc/v4/spring/DefaultWebMvcConfigurerAdapter.java
@@ -2,16 +2,9 @@ package com.jslsolucoes.tagria.doc.v4.spring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import com.jslsolucoes.spring.converter.BigDecimalConverter;
-import com.jslsolucoes.spring.converter.IntegerConverter;
-import com.jslsolucoes.spring.converter.LocalDateConverter;
-import com.jslsolucoes.spring.converter.LocalDateTimeConverter;
-import com.jslsolucoes.spring.converter.LongConverter;
 
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.jslsolucoes.tagria.doc.v4.spring.**" })
@@ -25,16 +18,5 @@ public class DefaultWebMvcConfigurerAdapter implements WebMvcConfigurer {
 	internalResourceViewResolver.setSuffix(".jsp");
 	return internalResourceViewResolver;
     }
-    
-    @Override
-    public void addFormatters(FormatterRegistry formatterRegistry) {
-	LocalDateTimeConverter localDateTimeConverter = new LocalDateTimeConverter();
-	formatterRegistry.addConverter(new LocalDateConverter(localDateTimeConverter));
-	formatterRegistry.addConverter(localDateTimeConverter);
-	formatterRegistry.addConverter(new IntegerConverter());
-	formatterRegistry.addConverter(new LongConverter());
-	formatterRegistry.addConverter(new BigDecimalConverter());
-    }
 
-   
 }
