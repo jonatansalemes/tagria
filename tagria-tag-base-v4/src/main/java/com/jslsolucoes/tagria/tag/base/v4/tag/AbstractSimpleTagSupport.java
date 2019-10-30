cee097fa90ecce3e5e3d1dd8d9e5252bf5ef0fd4
jonatan <jonatan@jslsolucoes.com>
2019-10-30 19:11:12 -0200

    tg version

--- tagria-tag-base-v4/src/main/java/com/jslsolucoes/tagria/tag/base/v4/tag/AbstractSimpleTagSupport.java
+++ tagria-tag-base-v4/src/main/java/com/jslsolucoes/tagria/tag/base/v4/tag/AbstractSimpleTagSupport.java
@@ -53,7 +53,7 @@ public abstract class AbstractSimpleTagSupport extends SimpleTagSupport implemen
     protected String id;
 
     private String version() {
-	return "4.0.0.24";
+	return "4.0.2.0";
     }
 
     private JspWriter writer() {
