33dae1bd736e0ca03d7f6e75488bd6353a7a7658
Jonatan Lemes <jonatan@jlsolucoes.com>
2019-10-31 20:59:39 -0300

    remove force change

--- tagria-tag-html-v4/src/main/resources/com/jslsolucoes/tagria/v4/lib/js/tagria/jquery.fix.js
+++
@@ -1,9 +0,0 @@
-(function($){
-    var originalVal = $.fn.val;
-    $.fn.val = function(){
-        var result =originalVal.apply(this,arguments);
-        if(arguments.length > 0)
-            $(this).change();
-        return result;
-    };
-})(jQuery);
