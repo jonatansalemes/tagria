799944d16f0843bf22cc8d64e1a6a13f6eb48fa9
Jonatan Lemes <jonatan@jslsolucoes.com>
2020-01-31 04:49:42 -0200

    new print component, fix exporter grid, fix charset on exportation file,support flush and horizontal list group

--- tagria-config-v4/src/main/java/com/jslsolucoes/tagria/config/v4/TagriaConfig.java
+++ tagria-config-v4/src/main/java/com/jslsolucoes/tagria/config/v4/TagriaConfig.java
@@ -14,7 +14,7 @@ public class TagriaConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(TagriaConfig.class);
     private static TagriaXML xml;
-    public static final String VERSION = "4.0.15.0";
+    public static final String VERSION = "4.0.15.3";
 
     private TagriaConfig() {
 
--- tagria-tag-html-v4/src/main/java/com/jslsolucoes/tagria/tag/html/v4/tag/listgroup/ListGroupTag.java
+++ tagria-tag-html-v4/src/main/java/com/jslsolucoes/tagria/tag/html/v4/tag/listgroup/ListGroupTag.java
@@ -7,20 +7,49 @@ import com.jslsolucoes.tagria.html.v4.Attribute;
 import com.jslsolucoes.tagria.html.v4.Element;
 import com.jslsolucoes.tagria.html.v4.ElementCreator;
 import com.jslsolucoes.tagria.tag.base.v4.tag.AbstractSimpleTagSupport;
+
 public class ListGroupTag extends AbstractSimpleTagSupport {
 
-	@Override
-	public Element render() {
+    private Boolean flushable = Boolean.FALSE;
+    private Boolean horizontal = Boolean.FALSE;
 
-		return ul();
+    @Override
+    public Element render() {
+
+	return ul();
+    }
+
+    private Element ul() {
+	Element ul = ElementCreator.newUl().attribute(Attribute.CLASS, "list-group").add(bodyContent());
+	if (!StringUtils.isEmpty(cssClass)) {
+	    ul.attribute(Attribute.CLASS, cssClass);
 	}
 
-	private Element ul() {
-		Element ul = ElementCreator.newUl().attribute(Attribute.CLASS, "list-group").add(bodyContent());
-		if (!StringUtils.isEmpty(cssClass)) {
-			ul.attribute(Attribute.CLASS, cssClass);
-		}
-		return ul;
+	if (flushable) {
+	    ul.attribute(Attribute.CLASS, "list-group-flush");
 	}
+
+	if (horizontal) {
+	    ul.attribute(Attribute.CLASS, "list-group-horizontal");
+	}
+
+	return ul;
+    }
+
+    public Boolean getFlushable() {
+	return flushable;
+    }
+
+    public void setFlushable(Boolean flushable) {
+	this.flushable = flushable;
+    }
+
+    public Boolean getHorizontal() {
+	return horizontal;
+    }
+
+    public void setHorizontal(Boolean horizontal) {
+	this.horizontal = horizontal;
+    }
 
 }
