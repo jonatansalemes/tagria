4c73cf76944d098a78d876283b409c24eb80f3e9
Jonatan Lemes <jonatan@jslsolucoes.com>
2020-07-27 20:37:25 -0300

    next develop iteration

--- tagria-api-v4/src/main/java/com/jslsolucoes/tagria/api/v4/Tagria.java
+++ tagria-api-v4/src/main/java/com/jslsolucoes/tagria/api/v4/Tagria.java
@@ -5,7 +5,7 @@ import com.jslsolucoes.cache.CacheInstanceBuilder;
 
 public class Tagria {
 
-    private static final String VERSION = "4.0.32.0";
+    private static final String VERSION = "4.0.33.0";
     private static final CacheInstance<String, Object> CACHE = CacheInstanceBuilder.newBuilder().withKey("tagriaCache")
 	    .build();
     
