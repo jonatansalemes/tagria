5f00a9483fe82f16f1420b528bca9628200d70fb
Jonatan Lemes <jonatan@jslsolucoes.com>
2020-11-06 00:11:42 -0300

    prepare docker 4.0.36

--- tagria-docker/registry.sh
+++ tagria-docker/registry.sh
@@ -1,3 +1,3 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 396182636964.dkr.ecr.us-east-1.amazonaws.com/jslsolucoes/tagria
-docker tag jslsolucoes/tagria:4.0.31 396182636964.dkr.ecr.us-east-1.amazonaws.com/jslsolucoes/tagria:4.0.31
-docker push 396182636964.dkr.ecr.us-east-1.amazonaws.com/jslsolucoes/tagria:4.0.31
+docker tag jslsolucoes/tagria:4.0.36 396182636964.dkr.ecr.us-east-1.amazonaws.com/jslsolucoes/tagria:4.0.36
+docker push 396182636964.dkr.ecr.us-east-1.amazonaws.com/jslsolucoes/tagria:4.0.36
